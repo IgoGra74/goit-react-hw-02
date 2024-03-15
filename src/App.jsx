7a57e1904a374b4b86e2f3acb4aa9acbca144b15
IgoGra74 <142592937+IgoGra74@users.noreply.github.com>
2024-03-15 14:37:35 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,28 @@ const App = () => {
     bad: 0,
   });
 
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const positiveFeedbackPersantage = Math.round(
+    ((feedback.good + feedback.neutral) / totalFeedback) * 100
+  );
+
   const updateFeedback = feedbackType => {
     setFeedback(prevState => ({
       ...prevState,
       [feedbackType]: prevState[feedbackType] + 1,
     }));
   };
+
   return (
     <>
       <Description />
       <Options updateFeedback={updateFeedback} />
-
-      <Feedback feedback={feedback} />
+      <Feedback
+        feedback={feedback}
+        totalFeedback={totalFeedback}
+        positiveFeedbackPersantage={positiveFeedbackPersantage}
+      />
     </>
   );
 };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,9 +1,11 @@
-const Feedback = ({ feedback }) => {
+const Feedback = ({ feedback, totalFeedback, positiveFeedbackPersantage }) => {
   return (
     <div>
       <p>Good: {feedback.good}</p>
       <p>Neutral: {feedback.neutral}</p>
       <p>Bad: {feedback.bad}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {positiveFeedbackPersantage}%</p>
     </div>
   );
 };
