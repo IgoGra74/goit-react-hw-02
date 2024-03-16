bdac1da7a00de1ba0ec806cc4881fdf116590405
IgorGra1 <gis1@ukr.net>
2024-03-16 15:03:18 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import Description from './components/Description/Description';
 import Feedback from './components/Feedback/Feedback';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Options from './components/Options/Options';
+import Notification from './components/Notification/Notification';
 
 const App = () => {
-  const [feedback, setFeedback] = useState({
+  const initialFeedback = JSON.parse(localStorage.getItem('feedback')) || {
     good: 0,
     neutral: 0,
     bad: 0,
-  });
+  };
+  const [feedback, setFeedback] = useState(initialFeedback);
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
@@ -22,16 +28,26 @@ const App = () => {
       [feedbackType]: prevState[feedbackType] + 1,
     }));
   };
-
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
   return (
     <>
       <Description />
-      <Options updateFeedback={updateFeedback} />
-      <Feedback
-        feedback={feedback}
+      <Options
+        updateFeedback={updateFeedback}
         totalFeedback={totalFeedback}
-        positiveFeedbackPersantage={positiveFeedbackPersantage}
+        resetFeedback={resetFeedback}
       />
+      {totalFeedback !== 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedbackPersantage={positiveFeedbackPersantage}
+        />
+      ) : (
+        <Notification />
+      )}
     </>
   );
 };
